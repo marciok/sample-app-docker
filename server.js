a55868cdef7e509dd2622756f453f63d36696436
Marcio Klepacz <marcioklepacz@gmail.com>
2024-08-16 11:08:32 -0300

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,8 @@ const HOST = "0.0.0.0";
 
 // App
 const app = express();
-app.get("/", (req, res) => res.send("Hello World!"));
+const codename = "Smash Mouth"
+app.get("/", (req, res) => res.send(`Hello {codename}`));
 
 let server = app.listen(PORT, () => {
   console.log(`Running on http://${HOST}:${PORT}`);
